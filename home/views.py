1e119ed521fbd4e5996d4eefc4c90743e118d807
vista <vistaostadii11@gmail.com>
2026-09-06 17:01:12 +0330

    Add post update view and form

--- home/views.py
+++ home/views.py
@@ -3,6 +3,9 @@ from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from .forms import PostUpdateForm
+
+
 
 class HomeView(View):
     def get(self, request):
@@ -27,3 +30,20 @@ class PostDeleteView(LoginRequiredMixin, View):
         return redirect("home:home")
 
 
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostUpdateForm
+
+    def dispatch(self, request, *args, **kwargs):
+        post = Post.objects.get(pk=kwargs["post_id"])
+        if post.user.id != request.user.id:
+            messages.error(request, "You can't update this post!", "danger")
+            return redirect("home:home")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        form = self.form_class(instance=post)
+        return render(request, "home/update.html", {"form": form})
+
+    def post(self, request, post_id):
+        pass
