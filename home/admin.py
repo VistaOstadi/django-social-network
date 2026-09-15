0f23a9e7bd475051eda15ba311c700fb736fd5ff
vista <vistaostadii11@gmail.com>
2026-09-15 18:07:28 +0330

    add comments to posts

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -9,5 +9,9 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":("body",)}
     raw_id_fields = ("user",)
 
-
 #admin.site.register(Post, PostAdmin )
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "post", "is_reply", "created")
+    raw_id_fields = ("user", "post", "reply")
