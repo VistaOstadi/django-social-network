1db42a8ce1b624b3d64a0eb3cd58ba01fc926248
vista <vistaostadii11@gmail.com>
2026-09-19 23:55:38 +0330

    add title field to Post model

--- home/models.py
+++ home/models.py
@@ -14,11 +14,13 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     body = models.TextField()
     slug = models.SlugField()
+    title = models.CharField(max_length=100, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created", "body"]
+
 
     def __str__(self):
         return f"{self.slug}-{self.created}"
