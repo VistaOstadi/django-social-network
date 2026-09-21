009ca10a1e0d61154bcc0ff423a9026e8d5767f4
vista <vistaostadii11@gmail.com>
2026-09-21 21:13:50 +0330

    add user profile editing

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from .models import Profile
 
 
 class UserRegisterForm(forms.Form):
@@ -67,3 +68,12 @@ class UserLoginForm(forms.Form):
             "class" :"form-control",
         })
     )
+
+
+class EditUserForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = Profile
+        fields = ("age", "address", )
+
--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views import View
-from .forms import UserRegisterForm, UserLoginForm
+from .forms import UserRegisterForm, UserLoginForm, EditUserForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -112,3 +112,21 @@ class UserUnfollowView(LoginRequiredMixin, View):
         else:
             messages.error(request, f"You are not following {user.username}!", "danger")
         return redirect("account:user_profile", user.id)
+
+
+class EditUserView(LoginRequiredMixin, View):
+    form_class = EditUserForm
+
+    def get(self, request):
+        form = self.form_class(instance=request.user.profile, initial={"email":request.user.email})
+        return render(request, "account/edit_profile.html", {"form":form})
+
+    def post(self, request):
+        form = self.form_class(request.POST, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            request.user.email = form.cleaned_data["email"]
+            request.user.save()
+            messages.success(request, "You have successfully edited your profile!", "success")
+        return redirect("account:user_profile", request.user.id)
+
